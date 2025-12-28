1926579537cdd2dc4ec89142a40fdcca1b663eb8
TawhidBepari <tawhidbepari2005@gmail.com>
2025-12-28 20:27:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const products = [
+  {
+    name: "Test Product",
+    description: "This is a test product",
+    price: "৳2542",
+    buy_url: "https://test.checkout.dodopayments.com/buy/pdt_ANjXatzKvUuliwpfkodzs?quantity=1"
+  }
+];
+
+const grid = document.getElementById("product-grid");
+
+products.forEach(p => {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <h3>${p.name}</h3>
+    <p>${p.description}</p>
+    <strong>${p.price}</strong><br><br>
+    <a href="${p.buy_url}" target="_blank">Buy</a>
+  `;
+  grid.appendChild(div);
+});
 
